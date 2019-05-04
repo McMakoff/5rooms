72eba7c73d2d27b180ff497a7ec58c88dba2374d
Мак <twopapavers@gmail.com>
2019-05-04 18:31:04 +0600

    Закончил шапку и баннер, осталось доделать модальные окна

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,49 @@
 jQuery(document).ready(function($){
 
-	$('.navigation__btn').click(function(){
-		if($('.navigation__wrap').css('display') == 'none') {
-			$('.navigation__wrap').show(700);
-			$('.navigation__btn').addClass('navigation__btn_open');
-		} else {
-			$('.navigation__wrap').hide(700);
-			$('.navigation__btn').removeClass('navigation__btn_open');
-		}
-	});
+	/*Мобильное меню*/
+	function navigation_btn_click (nav_btn, nav_btn_open, nav_wrap, nav_wrap_mobile) {
 
-	$(document).mouseup(function (e){
-		if (!$('.navigation__wrap').is(e.target) && $('.navigation__wrap').has(e.target).length === 0) {
-			$('.navigation__wrap').hide(700);
-			$('.navigation__btn').removeClass('navigation__btn_open');
-		}
-	});
+		$(window).resize(function() {
+			if(nav_btn.is(':visible')) {
+				nav_wrap.addClass(nav_wrap_mobile);
+			} else if(nav_btn.is(':hidden')) {
+				nav_wrap.removeClass(nav_wrap_mobile);
+			}
+		});
 
-	$('input').focus(function(){
-		$(this).parent().find('label').css('display','none');
-	});
-	$('input').blur(function(){
-		if($(this).val() == "") {
-			$(this).parent().find('label').css('display','block');
+		if(nav_btn.is(':visible')) {
+			nav_wrap.addClass(nav_wrap_mobile);
+		} else if(nav_btn.is(':hidden')) {
+			nav_wrap.removeClass(nav_wrap_mobile);
 		}
-	});
 
-	function btn_click (bttn, container){
+		$(document).mouseup(function (e){
+			if (!$('.' + nav_wrap_mobile).is(e.target) && $('.' + nav_wrap_mobile).has(e.target).length === 0) {
+				$('.' + nav_wrap_mobile).hide(700);
+				nav_btn.removeClass(nav_btn_open);
+			}
+		});
+
+		nav_btn.click(function(){
+			if($('.' + nav_wrap_mobile).css('display') == 'none') {
+				$('.' + nav_wrap_mobile).show(700);
+				nav_btn.addClass(nav_btn_open);
+			} else {
+				$('.' + nav_wrap_mobile).hide(700);
+				nav_btn.removeClass(nav_btn_open);
+			}
+		});
+	};
+
+	var nav_btn_var = $('.navigation__btn'),
+		nav_btn_open_var = 'navigation__btn_open',
+		nav_wrap_var = $('.navigation__list'),
+		nav_wrap_mobile_var = 'navigation__list_mobile';
+
+	navigation_btn_click (nav_btn_var, nav_btn_open_var, nav_wrap_var, nav_wrap_mobile_var);
+/*Мобильное меню конец*/
+
+	/*function btn_click (bttn, container){
 		bttn.click(function(){
 			if(container.css('display') == 'none'){
 				container.slideDown(300);
@@ -40,9 +57,37 @@ jQuery(document).ready(function($){
 				container.slideUp(300);
 			}
 		});
+	};*/
+
+	//btn_click($('.banner__order-btn'), $('.banner__order-modal'));
+
+	function modalOpen(btn_open, modal, btn_close) {
+		btn_open.click(function() {
+        	modal.slideToggle("fast");
+        });
+        btn_close.click(function(){
+        	modal.slideToggle("fast");
+        });
 	};
 
-	btn_click($('.call-btn'), $('.call-btn__modal'));
-	btn_click($('.filter__btn'), $('.filter__list'));
+	var banner_order_open = $(".banner__order-btn"),
+		design_order_modal = $('#order-design'),
+		banner_close = $('#order-design-close'),
+		back_call_order_open = $(".back-call__btn"),
+		back_call_order_modal = $('#order-call'),
+		back_call_close = $('#order-call-close');
+
+
+	modalOpen(banner_order_open, design_order_modal, banner_close);
+	modalOpen(back_call_order_open, back_call_order_modal, back_call_close);
+
+	$('.slick-prev').offset({left:($('.container').offset().left + 15)});
+	$('.banner__count').offset({left:($('.container').offset().left + $('.slick-prev').width() + 30)});
+	$('.slick-next').offset({left:($('.container').offset().left + $('.banner__count').innerWidth() + $('.slick-prev').width() + 45)});
+	$(window).resize(function() {
+		$('.slick-prev').offset({left:($('.container').offset().left + 15)});
+		$('.banner__count').offset({left:($('.container').offset().left + $('.slick-prev').width() + 30)});
+		$('.slick-next').offset({left:($('.container').offset().left + $('.banner__count').innerWidth() + $('.slick-prev').width() + 45)});
+	});
 
 });
